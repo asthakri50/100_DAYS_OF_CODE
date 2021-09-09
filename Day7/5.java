4af03035aa9d9920fecb084310e49599b0be6ed5
Astha Kumari <astha.kri50@gmail.com>
2021-09-10 01:01:53 +0530

    Updated

--- Day7/5.java
+++ Day7/5.java
@@ -3,3 +3,27 @@
  * https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/
  * strings/883/
  */
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+                sb.append(ch);
+        }
+
+        s = sb.toString();
+        int n = s.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (s.charAt(i) != s.charAt(n - 1 - i))
+                return false;
+        }
+
+        return true;
+    }
+}
